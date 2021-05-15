e0a4e2b4c16ad2e32c2f727ea4505bbad7cf9651
Thuc Nguyen <johnthuc1997@gmail.com>
2021-05-15 15:04:06 +0700

    update

--- assets/js/validator.js
+++ assets/js/validator.js
@@ -2,28 +2,94 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const ValidatorRules = {
-  required(value) {
-    return value && undefined || 'Vui lòng nhập trường này';
+  required(value, msg = 'Vui lòng nhập trường này') {
+    return value && undefined || msg;
   },
-  email(value) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(value).toLowerCase()) && undefined || 'Email không hợp lệ';
+  email(value, msg = 'Email không hợp lệ') {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(value).toLowerCase()) && undefined || msg;
   },
-  min(value) {
-    return value.length <= min && undefined || 'Độ dài chuỗi vượt quá qui định';
+  min(min) {
+    return (value, msg = `Độ dài chuỗi tối thiểu từ ${min} kí tự`) => (value.length >= min && undefined || msg);
+  },
+  max(max) {
+    return (value, msg = `Độ dài chuỗi tối đa là ${max} kí tự`) => (value.length <= max && undefined || msg);
   },
 }
 
+const ValidatorEvents = {
+  Rules: ValidatorRules,
+  FuncRules: {},
+  ErrorRules: {},
+
+  onblur(elements) {
+    if (this.FuncRules && this.ErrorRules) {
+      elements?.forEach(element => {
+        element.onblur = (e) => {
+          console.log(e);
+        }
+      })
+    }
+  },
+  onlick() {
+
+  }
+};
+
+const ValidatorFuncs = {
+  Rules: ValidatorRules,
+
+  getErrorInRules(elements, callback) {
+    elements.forEach(e => {
+      let name = e.getAttribute('name');
+      let rules = [];
+      e.getAttribute('validate-errors').split('|').forEach(msgError => {
+        rules.push(msgError);
+      });
+      callback(name, rules);
+    });
+  },
+
+  getFuncInRules(elements, callback) {
+    elements.forEach(e => {
+      let name = e.getAttribute('name');
+      let rules = [];
+      e.getAttribute('validate-rules').split('|').forEach(rule => {
+        if (rule.includes(':')) {
+          let ruleInfo = rule.split(':');
+          rules.push(this.Rules[ruleInfo[0]](ruleInfo[1]));
+        } else {
+          rules.push(this.Rules[rule]);
+        }
+      });
+      callback(name, rules);
+    });
+  }
+}
+
 const Validator = (formSelector) => {
-  let formRules = {};
+  const Funcs = ValidatorFuncs;
+  const Events = ValidatorEvents;
 
-
+  let funcRules = {};
+  let msgErrors = {};
   let eForm = $(formSelector);
   if (eForm) {
-    let eInput = eForm.querySelectorAll('[name][rules]');
-    eInput.forEach(e => {
-      formRules[e.name] = e.getAttribute('rules');
+
+    let eInputs = eForm.querySelectorAll('[name][validate-rules]');
+    let eErrors = eForm.querySelectorAll('[name][validate-errors]');
+
+    Funcs.getFuncInRules(eInputs, (name, rules) => {
+      funcRules[name] = rules;
     });
-    console.log(formRules);
-  }
+    // console.log(funcRules);
+
+    Funcs.getErrorInRules(eErrors, (name, rules) => {
+      msgErrors[name] = rules;
+    });
+    // console.log(msgErrors);
+    Events.FuncRules = funcRules;
+    Events.ErrorRules = msgErrors;
+    Events.onblur(eInputs);
+  };
 }
