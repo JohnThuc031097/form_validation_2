40682cb49f4762f49500b907d2da889df8a25935
Thuc Nguyen <johnthuc1997@gmail.com>
2021-05-17 12:01:14 +0700

    update

--- assets/js/validator.js
+++ assets/js/validator.js
@@ -1,285 +1,294 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const ValidatorRules = {
-  required(msg = 'Vui lòng nhập trường này') {
-    return value => (value === '' ? msg : undefined);
-  },
+const ValidatorRules = (() => {
+  return {
+    required(msg = 'Vui lòng nhập trường này') {
+      return value => (value === '' ? msg : undefined);
+    },
 
-  email(msg = 'Email không hợp lệ') {
-    return value => {
-      const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return regex.test(String(value).toLowerCase()) ? undefined : msg;
-    }
-  },
+    email(msg = 'Email không hợp lệ') {
+      return value => {
+        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(String(value).toLowerCase()) ? undefined : msg;
+      }
+    },
 
-  min(msg = 'Độ dài chuỗi tối thiểu không đủ') {
-    return min => value => (value.length >= min ? undefined : msg);
-  },
+    min(msg = 'Độ dài chuỗi tối thiểu không đủ') {
+      return min => value => (value.length >= min ? undefined : msg);
+    },
 
-  max(msg = 'Độ dài chuỗi tối đa bị vượt quá') {
-    return max => value => (value.length <= max ? undefined : msg);
-  },
+    max(msg = 'Độ dài chuỗi tối đa bị vượt quá') {
+      return max => value => (value.length <= max ? undefined : msg);
+    },
 
-  confirmed(msg = 'Chuỗi nhập vào không khớp') {
-    return name => value => {
-      let valueConfirm = $(`[name="${name}"][validate-rules]`).value.trim();
-      return (value === valueConfirm ? undefined : msg)
-    }
-  },
-}
-
-const ValidatorFuncs = {
-  Rules: ValidatorRules,
-  NameRules: {},
-
-  getNameFunc(nameRule, nameFunc) {
-    return this.NameRules[nameRule].some(name => name === nameFunc);
-  },
-
-  setMessage(eBox, eMessage, msg) {
-    if (msg) {
-      eBox.classList.add('invalid');
-      eMessage.innerHTML = msg;
-    } else {
-      eBox.classList.remove('invalid');
-      eMessage.innerHTML = '';
-    }
-  },
-
-  getElementInElements(elements, callback) {
-    elements?.forEach(e => {
-      let name = e.getAttribute('name');
-      callback(name, e);
-    });
-  },
-
-  getMessageInRules(elements, callback) {
-    elements?.forEach(e => {
-      let name = e.getAttribute('name');
-      let rules = [];
-      e.getAttribute('validate-messages').split('|').forEach(msg => {
-        rules.push(msg);
-      });
-      callback(name, rules);
-    });
-  },
-
-  getNameInRules(elements, callback) {
-    elements?.forEach(e => {
-      let name = e.getAttribute('name');
-      let nameRules = [];
-      e.getAttribute('validate-rules').split('|').forEach(rule => {
-        if (rule.includes(':')) {
-          let ruleInfo = rule.split(':');
-          nameRules.push(ruleInfo[0]);
-        } else {
-          nameRules.push(rule);
-        }
-      });
-      callback(name, nameRules);
-    });
-  },
-
-  getFuncInRules(elements, msgRules, callback) {
-    elements?.forEach(e => {
-      let name = e.getAttribute('name');
-      let funcRules = [];
-      e?.getAttribute('validate-rules').split('|').forEach((rule, index) => {
-        if (rule.includes(':')) {
-          let ruleInfo = rule.split(':');
-          funcRules.push(this.Rules[ruleInfo[0]](msgRules[name][index])(ruleInfo[1]));
-        } else {
-          funcRules.push(this.Rules[rule](msgRules[name][index]));
-        }
-      });
-      callback(name, funcRules);
-    });
+    confirmed(msg = 'Chuỗi nhập vào không khớp') {
+      return name => value => {
+        let valueConfirm = $(`[name="${name}"][validate-rules]`).value.trim();
+        return (value === valueConfirm ? undefined : msg)
+      }
+    },
   }
-}
+})
 
-const ValidatorEvents = {
-  Rules: ValidatorRules,
-  Funcs: ValidatorFuncs,
-  NameRules: {},
-  FuncRules: {},
-
-  onsubmit(elements, callbackMsg, calbackResults) {
-    if (this.FuncRules) {
-      let acceptSubmit = true;
-      let dataSubmit = {};
-      this.Funcs.getElementInElements(elements, (name, element) => {
-        let isRequired = this.Funcs.getNameFunc(name, 'required');
-        let msg = undefined;
-        let value = element.value;
-        let type = element.getAttribute('type');
-        if (isRequired) {
-          switch (type) {
-            case 'radio':
-              let eRadios = element.querySelectorAll('[name][type]');
-              [...eRadios].forEach(e => {
-                if (e.checked) return (value = e.value);
-              });
-              this.FuncRules[name].some(func => {
-                return (msg = func(!value && ''));
-              });
-              break;
-            case 'checkbox':
-              let eCheckboxs = element.querySelectorAll('[name][type]');
-              value = [...eCheckboxs].reduce((totalvalue, e) => {
-                return e.checked ? [...totalvalue, e.value] : totalvalue;
-              }, []);
-              this.FuncRules[name].some(func => {
-                return (msg = func(value.length > 0 && value || ''));
-              });
-              break;
-            default:
-              this.FuncRules[name].some(func => {
-                return (msg = func(value.trim()));
-              });
-              break;
-          }
-          msg && (acceptSubmit = false);
-        }
-        acceptSubmit && (dataSubmit[name] = value);
-        callbackMsg(name, msg);
+const ValidatorFuncs = (() => {
+  const _rules = ValidatorRules();
+  let _nameRules = undefined;
+  return {
+    init(nameRules) {
+      _nameRules = nameRules;
+    },
+    getNameFunc(nameRule, nameFunc) {
+      return _nameRules[nameRule].some(name => name === nameFunc);
+    },
+    setMessage(eBox, eMessage, msg) {
+      if (msg) {
+        eBox.classList.add('invalid');
+        eMessage.innerHTML = msg;
+      } else {
+        eBox.classList.remove('invalid');
+        eMessage.innerHTML = '';
+      }
+    },
+    getElementInElements(elements, callback) {
+      elements?.forEach(e => {
+        let name = e.getAttribute('name');
+        callback(name, e);
       });
-      calbackResults(acceptSubmit && dataSubmit);
+    },
+    getMessageInRules(elements, callback) {
+      elements?.forEach(e => {
+        let name = e.getAttribute('name');
+        let rules = [];
+        e.getAttribute('validate-messages').split('|').forEach(msg => {
+          rules.push(msg);
+        });
+        callback(name, rules);
+      });
+    },
+    getNameInRules(elements, callback) {
+      elements?.forEach(e => {
+        let name = e.getAttribute('name');
+        let nameRules = [];
+        e.getAttribute('validate-rules').split('|').forEach(rule => {
+          if (rule.includes(':')) {
+            let ruleInfo = rule.split(':');
+            nameRules.push(ruleInfo[0]);
+          } else {
+            nameRules.push(rule);
+          }
+        });
+        callback(name, nameRules);
+      });
+    },
+    getFuncInRules(elements, msgRules, callback) {
+      elements?.forEach(e => {
+        let name = e.getAttribute('name');
+        let funcRules = [];
+        e?.getAttribute('validate-rules').split('|').forEach((rule, index) => {
+          if (rule.includes(':')) {
+            let ruleInfo = rule.split(':');
+            funcRules.push(_rules[ruleInfo[0]](msgRules[name][index])(ruleInfo[1]));
+          } else {
+            funcRules.push(_rules[rule](msgRules[name][index]));
+          }
+        });
+        callback(name, funcRules);
+      });
     }
-  },
-  onblur(elements, callback) {
-    if (this.FuncRules) {
-      this.Funcs.getElementInElements(elements, (name, element) => {
-        let isRequired = this.Funcs.getNameFunc(name, 'required');
-        if (isRequired) {
-          element.onblur = () => {
+  }
+})
+
+const ValidatorEvents = (() => {
+  const _funcs = ValidatorFuncs();
+  let _funcRules = undefined;
+  let _nameRules = undefined;
+
+  return {
+    init(funcRules, nameRules) {
+      _funcRules = funcRules;
+      _nameRules = nameRules;
+      _funcs.init(_nameRules);
+    },
+    onblur(elements, callback) {
+      if (_funcRules) {
+        _funcs.getElementInElements(elements, (name, element) => {
+          let isRequired = _funcs.getNameFunc(name, 'required');
+          if (isRequired) {
+            element.onblur = () => {
+              let msg = undefined;
+              _funcRules[name].some(func => {
+                return (msg = func(element.value.trim()));
+              });
+              callback(name, msg);
+            }
+          }
+        });
+      }
+    },
+    oninput(elements, callback) {
+      if (_funcRules) {
+        _funcs.getElementInElements(elements, (name, element) => {
+          element.oninput = () => {
+            let isRequired = _funcs.getNameFunc(name, 'required');
             let msg = undefined;
-            this.FuncRules[name].some(func => {
-              return (msg = func(element.value.trim()));
-            });
+            let value = element.value;
+            let type = element.getAttribute('type');
+            switch (type) {
+              case 'radio':
+                let eRadios = element.querySelectorAll('[name][type]');
+                value = [...eRadios].some(e => e.checked);
+                _funcRules[name].some(func => {
+                  return (msg = func(value));
+                });
+                break;
+              case 'checkbox':
+                let eCheckboxs = element.querySelectorAll('[name][type]');
+                value = [...eCheckboxs].reduce((totalvalue, e) => {
+                  return e.checked ? [...totalvalue, e.value] : totalvalue;
+                }, []);
+                _funcRules[name].some(func => {
+                  return (msg = func(value));
+                });
+                break;
+              default:
+                _funcRules[name].some(func => {
+                  return (msg = func(value.trim()));
+                });
+                break;
+            }
+            if (!isRequired && value.trim() === '') {
+              msg = '';
+            }
             callback(name, msg);
           }
-        }
-      });
-    }
-  },
-  oninput(elements, callback) {
-    if (this.FuncRules) {
-      this.Funcs.getElementInElements(elements, (name, element) => {
-        element.oninput = () => {
-          let isRequired = this.Funcs.getNameFunc(name, 'required');
+        });
+      }
+    },
+    onsubmit(elements, callbackMsg, calbackResults) {
+      if (_funcRules) {
+        let acceptSubmit = true;
+        let dataSubmit = {};
+        _funcs.getElementInElements(elements, (name, element) => {
+          let isRequired = _funcs.getNameFunc(name, 'required');
           let msg = undefined;
           let value = element.value;
           let type = element.getAttribute('type');
-          switch (type) {
-            case 'radio':
-              let eRadios = element.querySelectorAll('[name][type]');
-              value = [...eRadios].some(e => e.checked);
-              this.FuncRules[name].some(func => {
-                return (msg = func(value));
-              });
-              break;
-            case 'checkbox':
-              let eCheckboxs = element.querySelectorAll('[name][type]');
-              value = [...eCheckboxs].reduce((totalvalue, e) => {
-                return e.checked ? [...totalvalue, e.value] : totalvalue;
-              }, []);
-              this.FuncRules[name].some(func => {
-                return (msg = func(value));
-              });
-              break;
-            default:
-              this.FuncRules[name].some(func => {
-                return (msg = func(value.trim()));
-              });
-              break;
+          if (isRequired) {
+            switch (type) {
+              case 'radio':
+                let eRadios = element.querySelectorAll('[name][type]');
+                [...eRadios].forEach(e => {
+                  if (e.checked) return (value = e.value);
+                });
+                _funcRules[name].some(func => {
+                  return (msg = func(!value && ''));
+                });
+                break;
+              case 'checkbox':
+                let eCheckboxs = element.querySelectorAll('[name][type]');
+                value = [...eCheckboxs].reduce((totalvalue, e) => {
+                  return e.checked ? [...totalvalue, e.value] : totalvalue;
+                }, []);
+                _funcRules[name].some(func => {
+                  return (msg = func(value.length > 0 && value || ''));
+                });
+                break;
+              default:
+                _funcRules[name].some(func => {
+                  return (msg = func(value.trim()));
+                });
+                break;
+            }
+            msg && (acceptSubmit = false);
           }
-          if (!isRequired && value.trim() === '') {
-            msg = '';
-          }
-          callback(name, msg);
-        }
-      });
-    }
+          acceptSubmit && (dataSubmit[name] = value);
+          callbackMsg(name, msg);
+        });
+        calbackResults(acceptSubmit && dataSubmit);
+      }
+    },
   }
-};
+})
 
-const Validator = {
-  Funcs: ValidatorFuncs,
-  Events: ValidatorEvents,
+const Validator = (() => {
+  const _funcs = ValidatorFuncs();
+  const _events = ValidatorEvents();
 
-  nameRules: {},
-  funcRules: {},
-  msgRules: {},
+  const _nameRules = {};
+  const _funcRules = {};
+  const _msgRules = {};
 
-  eBoxRules: {},
-  eMessageRules: {},
+  const _eBoxRules = {};
+  const _eMessageRules = {};
 
-  eForm: undefined,
-  eBoxs: undefined,
-  eMessages: undefined,
-  eInputs: undefined,
-  eRadios: undefined,
+  let _eForm = undefined;
+  let _eBoxs = undefined;
+  let _eMessages = undefined;
+  let _eInputs = undefined;
 
-  init(formSelector) {
-    this.eForm = $(formSelector);
-    if (this.eForm) {
-      this.eBoxs = this.eForm.querySelectorAll('[name][validate-boxs]');
-      this.eMessages = this.eForm.querySelectorAll('[name][validate-messages]');
-      this.eInputs = this.eForm.querySelectorAll('[name][validate-rules]');
+  function _initElements() {
 
-      this.initElements();
-      this.handleEvents();
-    }
-  },
-
-  initElements() {
-    this.Funcs.NameRules = this.nameRules;
-
-    this.Funcs.getElementInElements(this.eBoxs, (name, element) => {
-      this.eBoxRules[name] = element;
+    _funcs.getElementInElements(_eBoxs, (name, element) => {
+      _eBoxRules[name] = element;
     });
     //console.log(this.eBoxRules);
 
-    this.Funcs.getElementInElements(this.eMessages, (name, element) => {
-      this.eMessageRules[name] = element;
+    _funcs.getElementInElements(_eMessages, (name, element) => {
+      _eMessageRules[name] = element;
     });
     //console.log(this.eMessageRules);
 
-    this.Funcs.getMessageInRules(this.eMessages, (name, rules) => {
-      this.msgRules[name] = rules;
+    _funcs.getMessageInRules(_eMessages, (name, rules) => {
+      _msgRules[name] = rules;
     });
     //console.log(this.msgRules);
 
-    this.Funcs.getFuncInRules(this.eInputs, this.msgRules, (name, rules) => {
-      this.funcRules[name] = rules;
+    _funcs.getFuncInRules(_eInputs, _msgRules, (name, rules) => {
+      _funcRules[name] = rules;
     });
     //console.log(this.funcRules);
 
-    this.Funcs.getNameInRules(this.eInputs, (name, rules) => {
-      this.nameRules[name] = rules;
+    _funcs.getNameInRules(_eInputs, (name, rules) => {
+      _nameRules[name] = rules;
     });
     //console.log(nameRules);
-  },
+  }
 
-  handleEvents() {
-    this.Events.NameRules = this.nameRules;
-    this.Events.FuncRules = this.funcRules;
-
-    this.Events.onblur(this.eInputs, (name, msg) => {
-      this.Funcs.setMessage(this.eBoxRules[name], this.eMessageRules[name], msg);
+  function _handleEvents() {
+    _events.onblur(_eInputs, (name, msg) => {
+      _funcs.setMessage(_eBoxRules[name], _eMessageRules[name], msg);
     });
 
-    this.Events.oninput(this.eInputs, (name, msg) => {
-      this.Funcs.setMessage(this.eBoxRules[name], this.eMessageRules[name], msg);
-    });
-  },
-
-  getDataSubmit(callback) {
-    this.Events.onsubmit(this.eInputs, (name, msg) => {
-      this.Funcs.setMessage(this.eBoxRules[name], this.eMessageRules[name], msg);
-    }, results => {
-      callback(results);
+    _events.oninput(_eInputs, (name, msg) => {
+      _funcs.setMessage(_eBoxRules[name], _eMessageRules[name], msg);
     });
   }
-}
+
+  return {
+    init(formSelector) {
+      _eForm = $(formSelector);
+      if (_eForm) {
+        _eBoxs = _eForm.querySelectorAll('[name][validate-boxs]');
+        _eMessages = _eForm.querySelectorAll('[name][validate-messages]');
+        _eInputs = _eForm.querySelectorAll('[name][validate-rules]');
+
+        _funcs.init(_nameRules);
+        _initElements();
+
+        _events.init(_funcRules, _nameRules);
+        _handleEvents();
+      }
+    },
+    getFormElement() {
+      return _eForm;
+    },
+    getDataSubmit(callback) {
+      _events.onsubmit(_eInputs, (name, msg) => {
+        _funcs.setMessage(_eBoxRules[name], _eMessageRules[name], msg);
+      }, results => {
+        callback(results);
+      });
+    }
+  }
+});
